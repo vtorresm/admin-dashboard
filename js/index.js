b7b80f32be3a22e89b35529320f8e966fa05fc8e
vtorresm <vatorresm88@gmail.com>
2022-09-21 16:38:05 -0500

    Agregando la funcionalidad - Light & Dark Mode

--- js/index.js
+++ js/index.js
@@ -1,11 +1,21 @@
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
 
+// Show sidebar
 menuBtn.addEventListener("click", () => {
   sideMenu.style.display = "block";
 })
 
+// Close sidebar menu
 closeBtn.addEventListener("click", () => {
   sideMenu.style.display = "none";
 })
+
+// Change theme
+themeToggler.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme-variables");
+
+  themeToggler.querySelector("span").classList.toggle("active");
+})d
