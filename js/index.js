503cbecf1d37551cd8c4b96aac4660778730af51
vtorresm <vatorresm88@gmail.com>
2022-09-22 17:09:49 -0500

    Llenar tabla con pedidos usando JavaScript

--- js/index.js
+++ js/index.js
@@ -1,21 +1,42 @@
-const sideMenu = document.querySelector("aside");
-const menuBtn = document.querySelector("#menu-btn");
-const closeBtn = document.querySelector("#close-btn");
-const themeToggler = document.querySelector(".theme-toggler");
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const themeToggler = document.querySelector('.theme-toggler');
 
 // Show sidebar
-menuBtn.addEventListener("click", () => {
-  sideMenu.style.display = "block";
-})
+menuBtn.addEventListener('click', () => {
+  sideMenu.style.display = 'block';
+});
 
 // Close sidebar menu
-closeBtn.addEventListener("click", () => {
-  sideMenu.style.display = "none";
-})
+closeBtn.addEventListener('click', () => {
+  sideMenu.style.display = 'none';
+});
 
 // Change theme
-themeToggler.addEventListener("click", () => {
-  document.body.classList.toggle("dark-theme-variables");
+themeToggler.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme-variables');
 
-  themeToggler.querySelector("span").classList.toggle("active");
-})d
+  themeToggler.querySelector('span').classList.toggle('active');
+});
+
+// fill orders in table
+Orders.forEach((order) => {
+  const tr = document.createElement('tr');
+  const trContent = `
+    <td>${order.productName}</td>
+    <td>${order.productNumber}</td>
+    <td>${order.paymentStatus}</td>
+    <td class="${
+      order.shipping === 'Declined'
+        ? 'danger'
+        : order.shipping === 'Pending'
+        ? 'warning'
+        : 'primary'
+    }">${order.shipping}</td>
+    <td class="primary">Details</td>
+  `;
+
+  tr.innerHTML = trContent;
+  document.querySelector('table tbody').appendChild(tr);
+});
